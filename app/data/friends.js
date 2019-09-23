6b70ca2b492954c2f35afc06b5e8c971b3a99dfa
ThaboM82 <48286649+ThaboM82@users.noreply.github.com>
2019-09-23 13:48:28 +0200

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,88 @@
+// You should save your application's data inside of `app/data/friends.js` as an array of objects.
+//  Each of these objects should roughly follow the format below.
+// =============================================================
+
+var friends = [
+    {
+        "name": "Sergio",
+        "photo": "https://am11.akamaized.net/med/cnt/uploads/2010/01/Screen-shot-2010-01-31-at-11.47.04-AM-e1264957975704.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Baron Harkonnen",
+        "photo": "http://baskino.me/uploads/images/2013/104/oqnx12.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            3,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Hatchet Face",
+        "photo": "http://images.tmz.com/2016/09/15/0915-hatchet-face-kim-macguire-cry-baby-5.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            3,
+            3,
+            3,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Evil Lynn",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBCzJN2jYwe6IDoShgz21y5fvOVg-EbvQTfM1VzKnv6QvejX-E",
+        "scores": [
+            3,
+            5,
+            3,
+            5,
+            3,
+            5,
+            3,
+            5,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Queen Akasha",
+        "photo": "https://ace_hotel_dev.s3.amazonaws.com/images/queen_of_the_damned.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+];
+  // module exports for require to function correctly
+  module.exports = friends;
---
+++ app/routing/htmlroutes.js
@@ -0,0 +1,19 @@
+// Your `htmlRoutes.js` file should include two routes:
+var path = require("path");
+
+module.exports = function(app) {
+// * A GET Route to `/survey` which should display the survey page.
+// * A default, catch-all route that leads to `home.html` which displays the home page. 
+// Basic route that sends the user first to the AJAX Page
+  
+ 
+	// Home page
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '../public/home.html'));
+});
+
+// Survey page
+app.get('/survey', function(req, res) {
+	res.sendFile(path.join(__dirname, '../public/survey.html'));
+});
+};
